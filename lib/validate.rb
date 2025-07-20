2bed0611b3166d1ba3fa1debb92961bb4d72955f
Moazzam Shaikh <moazzamshaikh8982@gmail.com>
2025-07-21 00:03:53 +0530

    Update validate.rb

--- lib/validate.rb
+++ lib/validate.rb
@@ -1,12 +1,13 @@
 # Class contain validations
 class Validate
-  def initialize(chat_id)
+  def initialize(chat_id, redis)
     @chat_id = chat_id
+    @redis = redis
   end
 
   def affiliate_tags
-    Redis.current.exists?("#{@chat_id}:bitly_id") &&
-      Redis.current.exists?("#{@chat_id}:fkrt_id") &&
-      Redis.current.exists?("#{@chat_id}:amzn_id")
+    @redis.exists?("#{@chat_id}:bitly_id") &&
+      @redis.exists?("#{@chat_id}:fkrt_id") &&
+      @redis.exists?("#{@chat_id}:amzn_id")
   end
 end
