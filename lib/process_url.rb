20b61466f63e551a4df3b8fd15e99c9c4f7e76e4
Moazzam Shaikh <moazzamshaikh8982@gmail.com>
2025-07-21 00:04:16 +0530

    Update process_url.rb

--- lib/process_url.rb
+++ lib/process_url.rb
@@ -1,60 +1,56 @@
 require_relative 'bitlyurl'
 
 module ProcessUrl
-  class << self
-    attr_accessor :chat_id
-  end
-
-  def self.individual(url)
+  def self.individual(url:, chat_id:, redis:)
     case url
     when /amazon.in/
-      amazon(url, short: false)
+      amazon(url, chat_id, redis, short: false)
     when /amzn.to/, /amzn.in/
-      amazon(url, short: true)
+      amazon(url, chat_id, redis, short: true)
     when /flipkart.com/
-      flipkart(url, short: false)
+      flipkart(url, chat_id, redis, short: false)
     when /fkrt.it/
-      flipkart(url, short: true)
+      flipkart(url, chat_id, redis, short: true)
     else
-      redirection(url)
+      redirection(url, chat_id, redis)
     end
   end
 
-  def self.amazon(url, short: false)
+  def self.amazon(url, chat_id, redis, short: false)
     amazon = AffiliateProcess.new(url, 'tag')
     amazon.fetch_url if short
     amazon.clean_url
-    amzn_id = Redis.current.get("#{chat_id}:amzn_id")
+    amzn_id = redis.get("#{chat_id}:amzn_id")
     amazon.add_tracking_id(amzn_id)
-    shorten_url(amazon.updated_url)
+    shorten_url(amazon.updated_url, chat_id, redis)
   end
 
-  def self.flipkart(url, short: false)
+  def self.flipkart(url, chat_id, redis, short: false)
     flipkart = AffiliateProcess.new(url, 'affid')
     flipkart.fetch_url if short
     flipkart.clean_url
-    fkrt_id = Redis.current.get("#{chat_id}:fkrt_id")
+    fkrt_id = redis.get("#{chat_id}:fkrt_id")
     flipkart.add_tracking_id(fkrt_id)
-    shorten_url(flipkart.updated_url, flipkart: true)
+    shorten_url(flipkart.updated_url, chat_id, redis, flipkart: true)
   end
 
-  def self.redirection(url)
+  def self.redirection(url, chat_id, redis)
     url = get_redirected_url(url)
 
     found_url = match_first_url(url)
 
-    return individual(found_url) if found_url
+    return individual(url: found_url, chat_id: chat_id, redis: redis) if found_url
 
     return "URL Not Supported: #{url}" if url.is_a?(String)
 
-    return flipkart(url.request.last_uri) if url.request.last_uri.host.include? 'flipkart'
+    return flipkart(url.request.last_uri, chat_id, redis) if url.request.last_uri.host.include? 'flipkart'
 
-    return amazon(url.request.last_uri) if url.request.last_uri.host.include? 'amazon'
+    return amazon(url.request.last_uri, chat_id, redis) if url.request.last_uri.host.include? 'amazon'
 
     urls = URI.extract(url.parsed_response, %w[http https])
     flipkart = nil
     urls.each { |u| flipkart = u if u.include? 'flipkart' }
-    return flipkart(flipkart) unless flipkart.nil?
+    return flipkart(flipkart, chat_id, redis) unless flipkart.nil?
 
     url = get_redirected_url(urls[2]) if url.include? 'cashbackUrl'
     "URL Not Supported: #{url.is_a?(String) ? url : url.request.last_uri}"
@@ -74,13 +70,13 @@ module ProcessUrl
     "Error: #{e.message}: #{res&.request&.last_uri} #{url if res.nil?} "
   end
 
-  def self.shorten_url(url, flipkart: false)
+  def self.shorten_url(url, chat_id, redis, flipkart: false)
     if flipkart
       fkrt_url = "https://affiliate.flipkart.com/a_url_shorten?url=#{CGI.escape(url)}"
       res = HTTParty.get(fkrt_url, follow_redirects: false)
       res['response']['shortened_url']
     else
-      bitly_id = Redis.current.get("#{chat_id}:bitly_id")
+      bitly_id = redis.get("#{chat_id}:bitly_id")
       BitlyUrl.new(bitly_id, url).short_url
     end
   rescue StandardError => e
